54a139ced72acf3bd9868ca8e6cc6cc490280229
MargaretTechworld <margarettechworld@gmail.com>
2024-02-14 14:51:43 +0000

    Set up NaVbar

---
+++ src/components/Admission.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Admission = () => (
+  <div>
+    <h1>Admission</h1>
+  </div>
+);
+
+export default Admission;
---
+++ src/components/SupportUs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SupportUs = () => (
+  <div>
+    <h1>Support Us</h1>
+  </div>
+);
+
+export default SupportUs;
